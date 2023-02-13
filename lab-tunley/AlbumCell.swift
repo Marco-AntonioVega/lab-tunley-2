2ef4e0bc897f8e8a1de44370145fe23d4ee21f21
Marco-AntonioVega <27344marcovega@gmail.com>
2023-02-12 20:29:37 -0800

    Initial Commit

---
+++ lab-tunley/AlbumCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Marco-Antonio Vega on 2/12/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+}
